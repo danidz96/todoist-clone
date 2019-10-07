6407fe41e56c4a63941a81274d8b1b06d526b104
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-07 20:44:06 +0200

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+import { ICollatedTask } from '../model/ICollatedTask';
+export const collatedTasks: ICollatedTask[] = [
+	{ key: 'INBOX', name: 'Inbox' },
+	{ key: 'TODAY', name: 'Today' },
+	{ key: 'NEXT_7', name: 'Next 7 Days' }
+];
