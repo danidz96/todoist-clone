5b1bb6671416901d021a546675319285bd235e78
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-09 22:05:01 +0200

    create checkbox

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { firebase } from '../../firebase';
+import { ITask } from '../../model/ITask';
+
+interface Props {
+	task: ITask;
+}
+
+export const Checkbox: React.FC<Props> = ({ task }: Props) => {
+	const archiveTask = () => {
+		firebase.firestore().collection('tasks').doc(task.id).update({ archived: true });
+	};
+	return (
+		<div className="checkbox-holder" data-testid="checkbox-action" onClick={archiveTask}>
+			<span className="checkbox" />
+		</div>
+	);
+};
