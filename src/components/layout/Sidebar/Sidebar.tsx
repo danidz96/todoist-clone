c83fe109a35f2fa5a971ebe279ff3b3c47e8859d
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-12 00:00:56 +0200

    add sidebar styles

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa';
+import './Sidebar.scss';
 
 export const Sidebar: React.FC<{}> = () => {
 	return (
 		<div className="sidebar" data-testid="sidebar">
 			<ul className="sidebar__generic">
-				<li>
+				<li data-testid="inbox" className="inbox">
 					<span>
 						<FaInbox />
 					</span>
 					<span>Inbox</span>
 				</li>
-				<li>
+				<li data-testid="today" className="today">
 					<span>
 						<FaRegCalendar />
 					</span>
 					<span>Today</span>
 				</li>
-				<li>
+				<li data-testid="next_7" className="next_7">
 					<span>
 						<FaRegCalendarAlt />
 					</span>
