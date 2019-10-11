7a83759d680f312ff8b6aea687307932e30d85a5
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-12 00:00:46 +0200

    add content styles

--- src/components/layout/Content/Content.tsx
+++ src/components/layout/Content/Content.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Sidebar } from '../Sidebar/Sidebar';
 import { Tasks } from '../../Tasks/Tasks';
+import './Content.scss';
 
 interface Props {}
 
 export const Content: React.FC<Props> = () => {
 	return (
-		<section>
+		<section className="content">
 			<Sidebar />
 			<Tasks />
 		</section>
