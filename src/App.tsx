759e1b3869bff1a9d04817bcc2a3d1366145bd97
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-12 12:32:30 +0200

    create and use selectProjectContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Header } from './components/layout/Header/Header';
 import { Content } from './components/layout/Content/Content';
+import { ProjectsProvider, SelectedProjectProvider } from './context';
 import './App.scss';
 
 export const App: React.FC = () => {
 	return (
-		<div className="App">
-			<Header />
-			<Content />
-		</div>
+		<SelectedProjectProvider>
+			<ProjectsProvider>
+				<div className="App">
+					<Header />
+					<Content />
+				</div>
+			</ProjectsProvider>
+		</SelectedProjectProvider>
 	);
 };
---
+++ src/context/index.ts
@@ -0,0 +1,11 @@
+import { ProjectsContext, ProjectsProvider, useProjectsValue } from './projectsContext';
+import { SelectedProjectContext, SelectedProjectProvider, useSelectedProjectValue } from './selectedProjectProvider';
+
+export {
+	ProjectsContext,
+	ProjectsProvider,
+	useProjectsValue,
+	SelectedProjectContext,
+	SelectedProjectProvider,
+	useSelectedProjectValue
+};
