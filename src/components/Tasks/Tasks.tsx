23a262a4a85a58eaa09d81962f9a3e79d6534667
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-21 22:18:34 +0200

    update tasks

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Checkbox } from '../Checkbox/Checkbox';
 import { useTasks } from '../../hooks/useTasks';
+import { collatedTasks } from '../../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../../context/';
+
 import './Tasks.scss';
 
 interface Props {}
 
 export const Tasks: React.FC<Props> = () => {
-	const { tasks } = useTasks('1');
+	//@ts-ignore
+	const { selectedProject } = useSelectedProjectValue();
+	//@ts-ignore
+	const { projects } = useProjectsValue();
+	const { tasks } = useTasks(selectedProject);
 
 	let projectName: string = '';
+
+	if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+		projectName = getTitle(projects, selectedProject).name;
+		console.log('projectName 1', projectName);
+	}
+
+	if (collatedTasksExist(selectedProject) && selectedProject) {
+		projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+		console.log('projectName 2', projectName);
+	}
+
+	useEffect(() => {
+		document.title = `${projectName}: Todoist`;
+	});
 
 	return (
 		<div className="tasks" data-testid="tasks">
