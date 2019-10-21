2550c1fddc0887fe4da71c6280e30f6265b4b62b
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-21 22:18:47 +0200

    add helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,4 +1,8 @@
 import { collatedTasks } from '../constants';
 
+export const getTitle = (projects, projectId) => projects.find((project) => project.projectId === projectId);
+
+export const getCollatedTitle = (projects, key) => projects.find((project) => project.key === key);
+
 export const collatedTasksExist = (selectedProject: string) =>
 	collatedTasks.find((task) => task.key === selectedProject);
