6f50b8664845c5fbbbdd71e7e2455713e9ba0d26
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-26 13:43:54 +0200

    create addProject

---
+++ src/components/AddProject/AddProject.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { firebase } from '../../firebase';
+import { generatePushId } from '../../helpers/index';
+import { useProjectsValue } from '../../context';
+import uuid from 'uuid/v4';
+import './AddProject.scss';
+
+interface Props {
+	shouldShow?: boolean;
+}
+
+export const AddProject: React.FC<Props> = ({ shouldShow = false }) => {
+	const [ show, setShow ] = useState(shouldShow);
+	const [ projectName, setProjectName ] = useState('');
+	//@ts-ignore
+	const { setProjects } = useProjectsValue();
+
+	const projectId = uuid();
+	const addProject = () =>
+		projectName &&
+		firebase
+			.firestore()
+			.collection('projects')
+			.add({ projectId, name: projectName, userId: 'PIlfw5ql9ExmqixfenaK' })
+			.then(() => {
+				setProjects([]);
+				setProjectName('');
+				setShow(false);
+			});
+
+	return (
+		<div>
+			<div className="add-project" data-testid="add-project">
+				{show && (
+					<div className="add-project__input" data-testid="add-project-inner">
+						<input
+							value={projectName}
+							onChange={(e) => setProjectName(e.target.value)}
+							className="add-project__name"
+							data-testid="project-name"
+							type="text"
+							placeholder="Name your project"
+						/>
+						<button
+							className="add-project__submit"
+							type="button"
+							onClick={() => addProject()}
+							data-testid="add-project-submit"
+						>
+							Add Project
+						</button>
+						<span
+							aria-label="Cancel adding project"
+							data-testid="hide-project-overlay"
+							className="add-project__cancel"
+							onClick={() => setShow(false)}
+							onKeyDown={() => setShow(false)}
+							role="button"
+							tabIndex={0}
+						>
+							Cancel
+						</span>
+					</div>
+				)}
+				<span className="add-project__plus">+</span>
+				<span
+					aria-label="Add Project"
+					data-testid="add-project-action"
+					className="add-project__text"
+					onClick={() => setShow(!show)}
+					onKeyDown={() => setShow(!show)}
+					role="button"
+					tabIndex={0}
+				>
+					Add Project
+				</span>
+			</div>
+		</div>
+	);
+};
