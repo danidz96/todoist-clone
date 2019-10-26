045d1d1ae0ada1b52706fe0c80fc27930b760357
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-26 12:10:59 +0200

    set selected projects

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -14,19 +14,40 @@ export const Sidebar: React.FC<{}> = () => {
 	return (
 		<div className="sidebar" data-testid="sidebar">
 			<ul className="sidebar__generic">
-				<li data-testid="inbox" className="inbox">
+				<li
+					data-testid="inbox"
+					className={active === 'inbox' ? 'active' : undefined}
+					onClick={() => {
+						setActive('inbox');
+						setSelectedProject('INBOX');
+					}}
+				>
 					<span>
 						<FaInbox />
 					</span>
 					<span>Inbox</span>
 				</li>
-				<li data-testid="today" className="today">
+				<li
+					data-testid="today"
+					className={active === 'today' ? 'active' : undefined}
+					onClick={() => {
+						setActive('today');
+						setSelectedProject('TODAY');
+					}}
+				>
 					<span>
 						<FaRegCalendar />
 					</span>
 					<span>Today</span>
 				</li>
-				<li data-testid="next_7" className="next_7">
+				<li
+					data-testid="next_7"
+					className={active === 'next_7' ? 'active' : undefined}
+					onClick={() => {
+						setActive('next_7');
+						setSelectedProject('NEXT_7');
+					}}
+				>
 					<span>
 						<FaRegCalendarAlt />
 					</span>
