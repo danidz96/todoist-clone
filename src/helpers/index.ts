aafec72359924076866c736c51af467c651126cd
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-10-26 13:43:43 +0200

    install uuid

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,27 +6,3 @@ export const getCollatedTitle = (projects, key: string) => projects.find((projec
 
 export const collatedTasksExist = (selectedProject: string) =>
 	collatedTasks.find((task) => task.key === selectedProject);
-
-export const generatePushId = (() => {
-	const PUSH_CHARS = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
-
-	const lastRandChars = [];
-
-	return function() {
-		let now: number = new Date().getTime();
-
-		const timeStampChars = new Array(8);
-		for (var i = 7; i >= 0; i--) {
-			timeStampChars[i] = PUSH_CHARS.charAt(now % 64);
-			now = Math.floor(now / 64);
-		}
-
-		let id: string = timeStampChars.join('');
-
-		for (i = 0; i < 12; i++) {
-			id += PUSH_CHARS.charAt(lastRandChars[i]);
-		}
-
-		return id;
-	};
-})();
